b40d49085e87f38314d26e22428de26422c70f5d
Brian Moniz <brian@prohoods.com>
2018-07-04 11:59:25 -0600

    init commit for chess DnD ReactJS tutorial

---
+++ src/Board.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Square from './Square';
+import Knight from './Knight';
+
+export default class Board extends Component {
+  renderSquare(i) 
+  {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+    const black = (x + y) % 2 === 1;
+
+    const [knightX, knightY] = this.props.knightPosition;
+    // If, for this iteration knightX and knightY both equal x and y, draw
+    // the Knight, if not, it equals null and won't be drawn
+    const piece = (x === knightX && y === knightY) ? <Knight /> : null;
+
+      return(
+        <div key={i}
+             style={{ width: '12.5%', height: '12.5%'}}>
+             <Square black={black}>
+              {piece}
+             </Square>
+        </div>
+        );
+  }
+  render() 
+  {
+    const squares = [];
+    for (let i = 0; i < 64; i++)
+    {
+      // Pushes a fully drawn out and marked up square into the squares array
+      // For further processing, all loops must be outside of the return(); function
+      squares.push(this.renderSquare(i));
+    }
+
+    return (
+      <div style={{
+        width: '100%',
+        height: '700px',
+        display: "flex",
+        flexWrap: 'wrap'
+      }}>
+        {squares}
+      </div>
+      );
+
+  }
+}
+
+Board.propTypes = {
+  knightPosition: PropTypes.arrayOf(
+    PropTypes.number.isRequired
+  ).isRequired
+};
